b4b02679a56df0ffb98652aa28415e5ce2f535af
Sohrabbeig <ahsohrabbeig@gmail.com>
2023-12-03 00:17:49 -0700

    cleaned

--- manual_control.py
+++ manual_control.py
@@ -6,12 +6,6 @@ from minigrid.manual_control import ManualControl
 
 env = BaseEnv(map=map3, render_mode="human")
 
-# plot initial state
-observation, info = env.reset()
-img = env.render()
-plt.imshow(img)
-plt.savefig("initial_state.png")
-
 # enable manual control for testing
 manual_control = ManualControl(env, seed=42)
 manual_control.start()
