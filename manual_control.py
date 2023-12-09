cd34ff491a333ef2481200828c13053133dc7987
Sohrabbeig <ahsohrabbeig@gmail.com>
2023-12-08 19:37:52 -0700

    issue fixed

--- manual_control.py
+++ manual_control.py
@@ -1,11 +1,16 @@
-from matplotlib import pyplot as plt
 from envs import BaseEnv
 from maps import *
 from minigrid.manual_control import ManualControl
+from main import SharedList
 
 
-env = BaseEnv(map=map3, render_mode="human")
+class CustomManualControl(ManualControl):
+    def reset(self, seed=None):
+        self.env.reset(seed=seed, options=None)
+        self.env.render()
+
+env = BaseEnv(map_index=1, shared_list_actor=SharedList.remote(), render_mode="human")
 
 # enable manual control for testing
-manual_control = ManualControl(env, seed=42)
+manual_control = CustomManualControl(env, seed=42)
 manual_control.start()
