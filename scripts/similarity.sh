2a498c4130d95a4dd59bdc42c27601670e2bdc0d
Sohrabbeig <ahsohrabbeig@gmail.com>
2023-12-12 02:31:52 -0700

    output.txt for logging

--- scripts/similarity.sh
+++ scripts/similarity.sh
@@ -1,17 +1,19 @@
+> output.txt
+
 for seed in $(cat random_seeds.txt); do
     for distance in $(seq 1 2 20); do
-        python generate_map.py --seed $seed --target_distance $distance
+        python utils/generate_map.py --seed $seed --target_distance $distance > /dev/null 2>&1
 
         if [ $? -ne 0 ]; then
-            echo "Seed $seed - distance $distance failed: generate_map.py exited with status $?"
+            echo "Seed $seed - distance $distance failed: generate_map.py exited with status $?" >> output.txt
             continue
         fi
 
-        echo "Seed $seed - distance $distance: Single task"
-        python main.py --seed $seed --map_i=0 # single task
+        echo "Seed $seed - distance $distance: Single task" >> output.txt
+        python main.py --seed $seed --map_i=0 > /dev/null 2>&1 # single task 
 
-        echo "Seed $seed - distance $distance: Multi task"
-        python main.py --seed $seed --map_i=-1  # multi task
+        echo "Seed $seed - distance $distance: Multi task" >> output.txt
+        python main.py --seed $seed --map_i=-1 > /dev/null 2>&1 # multi task
     done
 done
 
