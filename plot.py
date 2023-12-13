c7c788b90ca756f360eec738e8a9b5714938f2e4
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2023-12-12 23:44:25 -0700

    functions for plotting interactions and algorithms added

--- plot.py
+++ plot.py
@@ -7,9 +7,12 @@ from pathlib import Path
 from envs import BaseEnv
 from utils.shared_list import SharedList
 
+NUM_SEEDS = 5
+
 
 def get_subdirectories(dir):
     return [entry.name for entry in Path(dir).iterdir() if entry.is_dir()]
+
 
 def plot_metric(dir, metric, map_i, y_axis='interactions'):
     '''
@@ -22,11 +25,11 @@ def plot_metric(dir, metric, map_i, y_axis='interactions'):
         subdir_path = os.path.join(dir, subdir)
         csv_file = os.path.join(subdir_path, get_subdirectories(subdir_path)[0], 'progress.csv')
         singleTask = True if subdir_path.split('/')[-1].split('_')[1] == 'SingleTask' else False
-        color = 'red' if singleTask else 'blue'
-            
+        color = 'red' if singleTask else 'green'
+
         if os.path.isfile(csv_file):
             df = pd.read_csv(csv_file)
-                
+
             col_reward = f'env{map_i}_{metric}'
             col_time_steps = f'env{map_i}_tts'
             if col_reward in df.columns:
@@ -35,12 +38,149 @@ def plot_metric(dir, metric, map_i, y_axis='interactions'):
                 elif y_axis == 'training steps':
                     plt.plot(df[col_reward], label='_'.join(subdir.split('_')[:2]), color=color)
 
+            display(df)
+            print("=====================================================")
+
     plt.xlabel(y_axis)
     plt.ylabel(metric)
     plt.title(f'env{map_i}')
     plt.legend()
     plt.show()
-    
+
+
+def plot_algorithms(dir, metric, map_i, y_axis='interactions'):
+    '''
+    mean_reward and mean_episode_length have only been implemented so far.
+    y_axis can be either 'interactions' or 'training steps'
+
+    exmaple: plot_metric('./results', 'mean_reward', 1, "training steps")
+    '''
+
+    dfs = pd.DataFrame()  # list to store dataframes
+    df_PPO = pd.DataFrame({})
+    df_IMPALA = pd.DataFrame({})
+    df_APPO = pd.DataFrame({})
+    current_algo = ''
+    algos = []
+    for subdir in get_subdirectories(dir):
+        subdir_path = os.path.join(dir, subdir)
+        csv_file = os.path.join(subdir_path, get_subdirectories(subdir_path)[0], 'progress.csv')
+        current_algo = subdir_path.split('/')[-1].split('_')[0]
+
+        new_algo = False if subdir_path.split('/')[-1].split('_')[0] not in algos else True
+
+        if new_algo:
+            print(current_algo)
+            print("new algo")
+            df_algo = pd.DataFrame({})
+
+        if os.path.isfile(csv_file):
+            df = pd.read_csv(csv_file)
+
+            col_reward = f'env{map_i}_{metric}'
+            if col_reward in df.columns:
+                if current_algo == 'PPO':
+                    df_PPO = pd.concat([df_PPO, df[[col_reward]]], axis=1)
+                elif current_algo == 'APPO':
+                    df_APPO = pd.concat([df_APPO, df[[col_reward]]], axis=1)
+                elif current_algo == 'IMPALA':
+                    df_IMPALA = pd.concat([df_IMPALA, df[[col_reward]]], axis=1)
+
+                # dfs.append(df[[col_time_steps, col_reward]])  # append dataframe into list
+            print("========================df before===========================")
+            display(dfs)
+
+            if df_PPO.columns.size == NUM_SEEDS:
+                df_PPO['PPO'] = df_PPO.mean(axis=1)
+                dfs = pd.concat([dfs, df_PPO[['PPO']]], axis=1)
+            elif df_APPO.columns.size == NUM_SEEDS:
+                df_APPO['APPO'] = df_APPO.mean(axis=1)
+                dfs = pd.concat([dfs, df_APPO[['APPO']]], axis=1)
+            elif df_IMPALA.columns.size == NUM_SEEDS:
+                df_IMPALA['IMPALA'] = df_IMPALA.mean(axis=1)
+                dfs = pd.concat([dfs, df_IMPALA[['IMPALA']]], axis=1)
+
+        # concatenate dataframes and calculate mean
+    print("========================df===========================")
+    display(dfs)
+
+    # df_concat = pd.concat(dfs)
+    # df_mean = df_concat.groupby(df_concat[col_time_steps]).mean()
+    # dfs['avg'] = dfs.mean(axis=1)
+    # df_average = dfs[['avg']]
+
+    print("=========================df mean============================")
+    # display(df_average)
+
+    # plot the average dataframe
+    # plt.plot(df_mean.index, df_mean[col_reward], color=color)
+    plt.plot(dfs.index, dfs)
+    plt.legend(list(dfs.columns))
+    plt.xlabel(y_axis)
+    plt.ylabel(metric)
+    plt.title(f'env{map_i}')
+    plt.show()
+
+
+def plot_interactions(dir, metric, map_i, y_axis='interactions'):
+    '''
+    mean_reward and mean_episode_length have only been implemented so far.
+    y_axis can be either 'interactions' or 'training steps'
+
+    exmaple: plot_metric('./results', 'mean_reward', 1, "training steps")
+    '''
+
+    dfs = pd.DataFrame()  # list to store dataframes
+    df_Multi = pd.DataFrame({})
+    df_Single = pd.DataFrame({})
+
+    for subdir in get_subdirectories(dir):
+        subdir_path = os.path.join(dir, subdir)
+        csv_file = os.path.join(subdir_path, get_subdirectories(subdir_path)[0], 'progress.csv')
+        current_multi = False if subdir_path.split('/')[-1].split('_')[1] == 'SingleTask' else True
+
+        if os.path.isfile(csv_file):
+            df = pd.read_csv(csv_file)
+
+            col_reward = f'env{map_i}_{metric}'
+            if col_reward in df.columns:
+                if current_multi:
+                    df_Multi = pd.concat([df_Multi, df[[col_reward]]], axis=1)
+                else:
+                    df_Single = pd.concat([df_Single, df[[col_reward]]], axis=1)
+
+            print("========================df before===========================")
+            display(dfs)
+
+            if df_Multi.columns.size == NUM_SEEDS:
+                df_Multi['multi'] = df_Multi.mean(axis=1)
+                dfs = pd.concat([dfs, df_Multi[['multi']]], axis=1)
+            elif df_Single.columns.size == NUM_SEEDS:
+                df_Single['single'] = df_Single.mean(axis=1)
+                dfs = pd.concat([dfs, df_Single[['single']]], axis=1)
+
+        # concatenate dataframes and calculate mean
+    print("========================df===========================")
+    display(dfs)
+
+    # df_concat = pd.concat(dfs)
+    # df_mean = df_concat.groupby(df_concat[col_time_steps]).mean()
+    # dfs['avg'] = dfs.mean(axis=1)
+    # df_average = dfs[['avg']]
+
+    print("=========================df mean============================")
+    # display(df_average)
+
+    # plot the average dataframe
+    # plt.plot(df_mean.index, df_mean[col_reward], color=color)
+    plt.plot(dfs.index, dfs)
+    plt.legend(list(dfs.columns))
+    plt.xlabel(y_axis)
+    plt.ylabel(metric)
+    plt.title(f'env{map_i}')
+    plt.show()
+
+
 def plot_map(map_index):
     env = BaseEnv(map_index=map_index, shared_list_actor=SharedList.remote(), render_mode="rgb_array")
 
