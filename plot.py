8291c59f3313451f947a1b75137cc7ed19bc6c73
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2023-12-12 23:49:32 -0700

    Redundant comments deleted

--- plot.py
+++ plot.py
@@ -49,30 +49,15 @@ def plot_metric(dir, metric, map_i, y_axis='interactions'):
 
 
 def plot_algorithms(dir, metric, map_i, y_axis='interactions'):
-    '''
-    mean_reward and mean_episode_length have only been implemented so far.
-    y_axis can be either 'interactions' or 'training steps'
-
-    exmaple: plot_metric('./results', 'mean_reward', 1, "training steps")
-    '''
-
     dfs = pd.DataFrame()  # list to store dataframes
     df_PPO = pd.DataFrame({})
     df_IMPALA = pd.DataFrame({})
     df_APPO = pd.DataFrame({})
-    current_algo = ''
-    algos = []
+
     for subdir in get_subdirectories(dir):
         subdir_path = os.path.join(dir, subdir)
         csv_file = os.path.join(subdir_path, get_subdirectories(subdir_path)[0], 'progress.csv')
         current_algo = subdir_path.split('/')[-1].split('_')[0]
-
-        new_algo = False if subdir_path.split('/')[-1].split('_')[0] not in algos else True
-
-        if new_algo:
-            print(current_algo)
-            print("new algo")
-            df_algo = pd.DataFrame({})
 
         if os.path.isfile(csv_file):
             df = pd.read_csv(csv_file)
@@ -86,10 +71,6 @@ def plot_algorithms(dir, metric, map_i, y_axis='interactions'):
                 elif current_algo == 'IMPALA':
                     df_IMPALA = pd.concat([df_IMPALA, df[[col_reward]]], axis=1)
 
-                # dfs.append(df[[col_time_steps, col_reward]])  # append dataframe into list
-            print("========================df before===========================")
-            display(dfs)
-
             if df_PPO.columns.size == NUM_SEEDS:
                 df_PPO['PPO'] = df_PPO.mean(axis=1)
                 dfs = pd.concat([dfs, df_PPO[['PPO']]], axis=1)
@@ -100,20 +81,8 @@ def plot_algorithms(dir, metric, map_i, y_axis='interactions'):
                 df_IMPALA['IMPALA'] = df_IMPALA.mean(axis=1)
                 dfs = pd.concat([dfs, df_IMPALA[['IMPALA']]], axis=1)
 
-        # concatenate dataframes and calculate mean
-    print("========================df===========================")
     display(dfs)
 
-    # df_concat = pd.concat(dfs)
-    # df_mean = df_concat.groupby(df_concat[col_time_steps]).mean()
-    # dfs['avg'] = dfs.mean(axis=1)
-    # df_average = dfs[['avg']]
-
-    print("=========================df mean============================")
-    # display(df_average)
-
-    # plot the average dataframe
-    # plt.plot(df_mean.index, df_mean[col_reward], color=color)
     plt.plot(dfs.index, dfs)
     plt.legend(list(dfs.columns))
     plt.xlabel(y_axis)
